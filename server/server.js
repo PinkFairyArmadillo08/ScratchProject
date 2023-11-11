dfaa18dfd9e2a81dd2609678753c1a89d4b2a74a
bisectors <gun.clawvuthinan@gmail.com>
2023-11-11 15:53:09 -0500

    app post and get

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const habitController = require('./controller/habitController.js')
 
 const app = express();
 
-const PORT = 3000;
 
+const PORT = 3000;
+console.log('dirname', __dirname)
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
 
@@ -13,6 +19,15 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
+
+app.post('/addHabit', habitController.addHabit, (req, res) => {
+  console.log('added')
+  return res.status(201).send('habit created successfully');
+})
+
+app.get('/getHabits', habitController.getHabits, (req, res) => {
+  return res.status(200).json({"user1": res.locals.allHabits});
+})
 
 //start up server on PORT
 app.listen(PORT, () => {
