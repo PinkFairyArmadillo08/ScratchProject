5acf9ab10bfc1558bdfcf6d2c6b9d4d7ceda069e
MXW9699 <mitchell.wen@hotmail.com>
2023-11-11 21:00:30 -0500

    fetch request (POST) to /login with username and password from input bars. expected to get back a boolean to either redirect or change state

--- components/Login-Signup-Container/LoginPage.jsx
+++ components/Login-Signup-Container/LoginPage.jsx
@@ -1,50 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const verifiedUser = { Mitch: 'Wen' };
 
 const LoginPage = () => {
-  const [loginSucess, setLoginSucess] = useState(true);
+  //React-router hook to change paths
+  const navigate = useNavigate();
 
+  /***************************STATES*************************************************/
+  const [loginSucess, setLoginSucess] = useState(true); //LOGIN STATUS
+  const [login, setLogin] = useState({ userName: '', password: '' }); //ENTRY FORM INFO FOR LOGIN
 
-  //handler function to sumbit login information
-  const loginClick = () => {
-    const username = document.getElementById('loginUsername').value;
-    const password = document.getElementById('loginPassword').value;
-    //base case if both fields are empty: do nothing
-    if (username == '' && password == '') return;
-
-    //GET request to see if user info is correct.
-    // fetch('/login')
-    // .then(data => data.json)
-    // .then(data =>{
-    //    //if invalid set the loginSucess to false
-    // if (verifiedUser[username] !== password) setLoginSucess(false);
-
-    //  })
-
-    //if invalid set the loginSucess to false
-    if (verifiedUser[username] !== password) setLoginSucess(false);
-    else setLoginSucess(true);
+  /************************HANDLER FUNCTIONS****************************************/
+  //Updating the inputs from userName and password stored as states
+  const populateLogin = (event) => {
+    setLogin(Object.assign(login, { [event.target.id]: event.target.value }));
   };
 
-  //buttons to redirect to signup page and home page
+  //handler function to submit login information
+  const loginClick = () => {
+    //base case if both fields are empty: do nothing
+    if (login.userName == '' || login.password == '') return;
+    //POST request to see if user info is correct.
+    fetch('/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userName: login.userName,
+        password: login.password,
+      }),
+    })
+      //data should come back as boolean
+      //if true go to HomePage; false set the loginSucess to false
+      .then((data) => data.json)
+      .then((data) => {
+        data ? navigate('/home') : setLoginSucess(false);
+      });
+  };
+
+  /*************************RENDER COMPONENT************************************************** */
   return (
     <div className="Login">
       <h1>WELCOME TO HABIT BUILDER</h1>
       <div id="usernameInput">
+        {/* conditional render for incorrecly entered login information */}
         {loginSucess ? <p></p> : <p>*Invalid username and password</p>}
-        <input id="loginUsername" type="text" placeholder="username" />
+        {/* USERNAME INPUTBAR */}
+        <input
+          id="userName"
+          type="text"
+          placeholder="username"
+          onChange={(e) => populateLogin(e)}
+        />
       </div>
-      <input id="loginPassword" type="password" placeholder="password" />
+      {/* PASSWORD INPUT BAR */}
+      <input
+        id="password"
+        type="password"
+        placeholder="password"
+        onChange={(e) => populateLogin(e)}
+      />
+      {/* Buttons to redirect to SignUpPage and HomePage */}
       <div className="LoginPageButtons">
-        <Link to={'/signup'}>
-          <button id="LoginButton">Signup</button>
-        </Link>
-        <button onClick={loginClick}></button>
-        <Link to={'/home'}>
-          <button id="SignupButton">Login</button>
-        </Link>
+        <button id="LoginButton" onClick={() => navigate('/signup')}>
+          Signup
+        </button>
+        <button id="SignupButton" onClick={loginClick}>
+          Login
+        </button>
       </div>
       <Outlet />
     </div>
--- components/Login-Signup-Container/SignUpPage.jsx
+++ components/Login-Signup-Container/SignUpPage.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const SignUpPage = () => {
+  const navigate = useNavigate();
   return (
     <div className="Login">
       <h1>Signing Up</h1>
       <input name="username" type="text" />
       <input name="password" type="password" />
-      <Link to={'/'}>
-        <button>Back to login</button>
-      </Link>
+      <button onClick={() => navigate('/')}>Back to login</button>
     </div>
   );
 };
