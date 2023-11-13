58cf59976802e4768612b067a3ec31ff131e5bcf
MXW9699 <mitchell.wen@hotmail.com>
2023-11-13 12:34:23 -0500

    split sign up container to its own file and have signup page as a presentational component; updated password requirements text.

--- components/Login-Signup-Container/LoginRoutes.jsx
+++ components/Login-Signup-Container/LoginRoutes.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './LoginPage.jsx';
-import SignUpPage from './SignUpPage.jsx';
 import HomePage from './HomePage.jsx';
 import './LoginSignupStyles.scss';
+import SignUpContainer from './SignUpContainter.jsx';
 
 const LoginRoutes = () => {
   //EACH PAGE SHOULD BE A CONTAINER COMPONENT AND HAVE THEIR OWN STATES AND HANDER FUNCTIONS
@@ -14,7 +14,7 @@ const LoginRoutes = () => {
       {/* path to the home page */}
       <Route path="/home" element={<HomePage />} />
       {/* path to the signup page  */}
-      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/signup" element={<SignUpContainer />} />
     </Routes>
   );
 };
