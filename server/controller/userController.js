6742e888c476d152b1f7592f7f7c2f188c80ba68
bisectors <gun.clawvuthinan@gmail.com>
2023-11-13 10:27:57 -0500

    done getoneuser function

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,6 +3,23 @@ const { User } = models
 
 const userController = {};
 
+//get one user function
+userController.getOneUser = async (req, res, next) => {
+  const userName = req.params.name
+  try {
+    const oneExistingUser = await User.find({userName:userName})
+    res.locals.oneExistingUser = oneExistingUser;
+    next()
+  }
+
+  catch(err) {
+    next({
+      error: 'error in getOneUser middleware',
+      message: 'cannot get user at the moment'
+    })
+  }
+}
+
 //signup function
 userController.signUp = async (req, res, next) => {
   const { userName, password } = req.body;
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,12 @@ const userController = require('../controller/userController');
 const router = express.Router();
 
 /************************ USERS (sign-up/log-in) *******************************/
+//get user
+router.get('/:name', userController.getOneUser, (req,res) => {
+  return res.status(201).send(res.locals.oneExistingUser);
+})
 //signup username and password
 router.post('/signUp', userController.signUp, (req, res) => {
-  console.log('user created222')
   return res.status(201).send(res.locals.newUser);
 });
 
