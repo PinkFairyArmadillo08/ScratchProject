b8ff94cc22e1a7f88861a3c31158b314aa74eb59
Jessica Grant <jessicarobyn10@gmail.com>
2023-11-13 18:12:07 -0500

    Updating fetches

--- components/HomePage/HomeContainer.jsx
+++ components/HomePage/HomeContainer.jsx
@@ -2,87 +2,76 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import AddHabitForm from './AddHabitForm.jsx';
 import HabitCard from './HabitCard.jsx';
+const cache = {};
 
 const HomeContainer = () => {
 
+  /**********************************STATES**********************************/
   const [habits, setHabits] = useState([]);
   const [popupOpen, setpopupOpen] = useState(false);
   const [newHabit, setNewHabit] = useState({'habitName': '', 'cue': '', 'reward': ''})
 
+  /*******************************USE EFFECT**********************************/
   // send a fetch to get the habit data from the backend
-  // data will be returned as an array of object with the following properties:
-  // sends cookie with username prop
+  // data will be returned as an array of objects with the following properties:
   // habitName, cue, rewards 
-  // useEffect(() => {
-  //   fetch('/habit/get')
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     setHabits(data);
-  //   })
-  // }, [])
-
-  // iterate over the habits array (which now contains the habit data) and render a habit card for each object
   useEffect(() => {
-    setHabits([
-      {'habitName': 'a', 'cue': 'a', 'reward': 'a'}, {'habitName': 'b', 'cue': 'b', 'reward': 'b'}, {'habitName': 'c', 'cue': 'c', 'reward': 'c'}, {'habitName': 'd', 'cue': 'd', 'reward': 'd'}, {'habitName': 'e', 'cue': 'e', 'reward': 'e'}])
-  }, []) 
-  // setHabits(newHabits);
+    console.log('initial fetch')
+    if (cache[0] != habits) {
+      fetch('/habit/get')
+      .then((res) => res.json())
+      .then((data) => {
+        cache[0] = data;
+        console.log(data);
+        setHabits(data);
+      })
+      .catch((err) => {console.log(err)})
+    }
+  }, [])
   
-  
+  /****************************EVENT HANDLERS*******************************/
   // click  handler that pushes a completed habit to the end of the queue
   const handleCompleted = (e, i) => {
     e.preventDefault();
-    console.log(e);
     let currentCard = habits[i];
     const updatedHabits = habits.slice(0, i).concat(habits.slice(i + 1).concat(currentCard))
-
-    console.log(updatedHabits);
     setHabits(updatedHabits);
-
-    //fetch('/habit/update')
   }
-    
-  
-  // useEffect(() => {
+
+    // iterate over the habits array (which now contains the habit data) and render a habit card for each object
     const renderingHabits = []
     for (let i = 0; i < habits.length; i++) {
       renderingHabits.push(
         <HabitCard habitObj={habits[i]} key={`Habit position: ${i}`} habitIndex={`${i}`} handleCompleted={handleCompleted} />
       )
     }
-  // }, [habits])
+
 
   // this function is connected to the "add habit" button, and renders the habit form pop-up when clicked
   // updates the state "popupOpen" from false to true when clicked
   // this function is also connected to the 'X' button on the habit pop-up form and it closes the form
   // use case: someone clicks on "add habit" but wants to close the pop-up without actually adding a habit
   const handleClick = () => {
-    console.log('opening and closing popup!') 
     setpopupOpen(!popupOpen)
   }
 
   // this function grabs the input data from the add habit form and updates the state with this data
   const handleChange = (e) => {
-    // console.log(e);
-    // console.log(e.target);
-    console.log(e.target.value);
-    console.log(newHabit);
     setNewHabit(Object.assign(newHabit, {[e.target.name]: e.target.value}))
   }
 
   // this function is connected to the submit button on the add habit pop-up form and it sends the new habit data to the backend and closes the form
-  // sends cookie with username prop
   const handleSubmit = () => {
     // sends post request to backend
-    console.log('clickedSubmit!')
-    console.log(newHabit);
-    // fetch('/habit/add'), {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json' 
-    //   },
-    //   body: JSON.stringify(newHabit)
-    // }
+    fetch('/habit/add', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(newHabit)
+    }).then((res) => res.json())
+      .then((data) => {console.log(data)})
+      .catch((err) => {
+        console.log(err)
+    })
   }
 
   return (
--- server/server.js
+++ server/server.js
@@ -7,17 +7,30 @@ app.use('/build', express.static(path.join(__dirname, '../build')));
 
 /*********************LOAD PAGES**************************************** */
 //main page get. send them the html file
+app.post('/login', (req, res) => {
+  const { userName, password } = req.body;
+  return res.status(200).json(userName == 'Mitch' && password == 'Wen');
+});
+app.post('/signup', (req, res) => {
+  const { userName, password } = req.body;
+  return res.status(200).json(true);
+});
+
+app.get('/habit/get', (req, res) => {
+  console.log('getting habits from the data base!')
+  return res.status(200).json([{"habitName": "a", 'cue': 'a', 'reward': 'a'}, {'habitName': 'b', 'cue': 'b', 'reward': 'b'}, {'habitName': 'c', 'cue': 'c', 'reward': 'c'}, {'habitName': 'd', 'cue': 'd', 'reward': 'd'}, {'habitName': 'e', 'cue': 'e', 'reward': 'e'}])
+})
+
 app.get('/*', (req, res) => {
  return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
-app.post('/login', (req, res) => {
- const { userName, password } = req.body;
- return res.status(200).json(userName == 'Mitch' && password == 'Wen');
+// {"habitName": "a", 'cue': 'a', 'reward': 'a'}, {'habitName': 'b', 'cue': 'b', 'reward': 'b'}, {'habitName': 'c', 'cue': 'c', 'reward': 'c'}, {'habitName': 'd', 'cue': 'd', 'reward': 'd'}, {'habitName': 'e', 'cue': 'e', 'reward': 'e'}])
+
+app.post('/habit/add', (req, res) => {
+  console.log('in the post request on the server')
+  return res.status(200).json(req.body)
 });
-app.post('/signup', (req, res) => {
- const { userName, password } = req.body;
- return res.status(200).json(true);
-});
+
 //start up server on PORT
 app.listen(PORT, () => {
  console.log(`Server listening on port: ${PORT}`);
